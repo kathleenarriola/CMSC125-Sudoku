97a7f8ff2a84b7ca7bc453e3d776028cec5a35bb
kathleenarriola <ksarriola@up.edu.ph>
2017-05-05 11:26:48 +0800

    Finished Randomizer

--- sudoku.c
+++ sudoku.c
@@ -4,10 +4,35 @@
 #define DIFFICULT 20
 #define SIZE 9
 
-void randomize(int n) {
-	while (n > 0) {
+int board[SIZE][SIZE] = { 0 };
 
+void randomize(int n) {
+	int row, col, value; 
+
+	while (n > 0) {
+		value = randomNum();
+		printf("%d\n", value);
+		do {
+			row = randomNum() - 1;
+ 	        col = randomNum() - 1;
+		} while (board[row][col] != 0);
+		printf("row: %d, col: %d\n", row, col);
+	    board[row][col] = value;
+	    //check 
+	    n--;
 	}
+}
+
+int randomNum() {
+    int n;
+    
+    n = rand() % 9 + 1;
+    
+    return n;
+}
+
+int randomCheck(int row, int col, int value) {
+
 }
 
 int isPresent(int element, int present[SIZE], int presentLastIndex) {
@@ -28,7 +53,6 @@ int lineCheck(int row[SIZE]) {
 	int present[SIZE];
 	int presentLastIndex = 0;
 
-	// horizontal
 	for (i = 0; i < SIZE; i++) {
 		if (isPresent(row[i], present, presentLastIndex) == -1) {
 			present[presentLastIndex] = row[i];
@@ -71,23 +95,29 @@ int checker(int answer[SIZE][SIZE]) {
 }
 
 int main() {
-	int board[SIZE][SIZE] = {
-		{9, 4, 2, 7, 6, 1, 8, 5, 3},
-		{3, 8, 7, 5, 9, 2, 6, 4, 1},
-		{6, 1, 5, 8, 3, 4, 2, 9, 7},
-		{2, 6, 3, 1, 4, 7, 5, 8, 9},
-		{8, 7, 1, 9, 2, 5, 3, 6, 4},
-		{4, 5, 9, 3, 8, 6, 1, 7, 2},
-		{7, 9, 6, 2, 1, 8, 4, 3, 5},
-		{, 2, 8, 4, 7, 3, 9, 1, 6},
-		{1, 3, 4, 6, 5, 9, 7, 2, 8}
-	};
+	// board[SIZE][SIZE] = { 0 }; // init board to 0
+	// board[SIZE][SIZE] = {
+	// 	{9, 4, 2, 7, 6, 1, 8, 5, 3},
+	// 	{3, 8, 7, 5, 9, 2, 6, 4, 1},
+	// 	{6, 1, 5, 8, 3, 4, 2, 9, 7},
+	// 	{2, 6, 3, 1, 4, 7, 5, 8, 9},
+	// 	{8, 7, 1, 9, 2, 5, 3, 6, 4},
+	// 	{4, 5, 9, 3, 8, 6, 1, 7, 2},
+	// 	{7, 9, 6, 2, 1, 8, 4, 3, 5},
+	// 	{, 2, 8, 4, 7, 3, 9, 1, 6},
+	// 	{1, 3, 4, 6, 5, 9, 7, 2, 8}
+	// };
 
 	//int arr[] = {1, 2, 9, 3, 4, 5, 6, 7, 8};
-	int ret = 0;
+	int i, j;
+	randomize(20); 
 
-	ret = checker(board);
-	printf("ret: %d\n", ret);
+	for (i = 0; i < SIZE; i++) {
+		for (j = 0; j < SIZE; j++) {
+			printf("%d\t", board[i][j]);
+		}	
+		printf("\n");
+	}
 
 	return 0;
 }
