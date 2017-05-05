389bbda0541872a885598e3e4f9f96a6368c363f
kathleenarriola <ksarriola@up.edu.ph>
2017-05-05 10:38:38 +0800

    Finished Board Checker.

--- sudoku.c
+++ sudoku.c
@@ -10,28 +10,6 @@ void randomize(int n) {
 	}
 }
 
-int checker(int answer[SIZE][SIZE]) {
-	int i = 0;
-	int j = 0;
-	int hCheck[SIZE];
-	int verticalCol[SIZE];
-	int vertColIndex = 0;
-
-	// horizontal
-	// for (i = 0; i < SIZE; i++) {
-	// 	lineCheck(answer[i]);
-	// }
-
-	// vertical
-	while (verticalColIndex != SIZE) {
-		for (i=0; i<SIZE; i++) {
-			verticalCol[i] = answer[i][vertColIndex];
-		}
-		lineCheck(verticalCol);
-		verticalColIndex++;
-	}
-}
-
 int isPresent(int element, int present[SIZE], int presentLastIndex) {
 	int i = 0;
 
@@ -63,13 +41,52 @@ int lineCheck(int row[SIZE]) {
 	return 1;
 }
 
+int checker(int answer[SIZE][SIZE]) {
+	int i = 0;
+	int j = 0;
+	int hCheck[SIZE];
+	int verticalCol[SIZE];
+	int vertColIndex = 0;
+
+	// horizontal
+	for (i = 0; i < SIZE; i++) {
+		if (lineCheck(answer[i]) == -1) {
+			return -1;
+		}
+	}
+
+	// vertical
+	while (vertColIndex != SIZE) {
+		for (i=0; i<SIZE; i++) {
+			verticalCol[i] = answer[i][vertColIndex];
+		}
+
+		if (lineCheck(verticalCol) == -1) {
+			return -1;
+		}
+		vertColIndex++;
+	}
+
+	return 1;
+}
+
 int main() {
-	printf("Hello\n");
-	int board[SIZE][SIZE];
-	int arr[] = {1, 2, 9, 3, 4, 5, 6, 7, 8};
+	int board[SIZE][SIZE] = {
+		{9, 4, 2, 7, 6, 1, 8, 5, 3},
+		{3, 8, 7, 5, 9, 2, 6, 4, 1},
+		{6, 1, 5, 8, 3, 4, 2, 9, 7},
+		{2, 6, 3, 1, 4, 7, 5, 8, 9},
+		{8, 7, 1, 9, 2, 5, 3, 6, 4},
+		{4, 5, 9, 3, 8, 6, 1, 7, 2},
+		{7, 9, 6, 2, 1, 8, 4, 3, 5},
+		{, 2, 8, 4, 7, 3, 9, 1, 6},
+		{1, 3, 4, 6, 5, 9, 7, 2, 8}
+	};
+
+	//int arr[] = {1, 2, 9, 3, 4, 5, 6, 7, 8};
 	int ret = 0;
 
-	ret = lineCheck(arr);
+	ret = checker(board);
 	printf("ret: %d\n", ret);
 
 	return 0;
